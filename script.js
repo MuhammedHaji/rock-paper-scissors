b3317063eb5eea41394852dac1e64bafcf81f6b5
MuhammedHaji <138349773+MuhammedHaji@users.noreply.github.com>
2025-10-18 16:02:34 +0300

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("hello world");
+//getComputerChoice:returns 'rock','paper' or 'scissors' randomly
+function getComputerChoice(){
+    const n=Math.floor(Math.random()*3);
+    if(n===0) return "rock";
+    if(n===1) return "paper"
+    return "scissors";}
+    //test
+console.log("computer choice:", getComputerChoice());
