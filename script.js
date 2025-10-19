6ed8267c55f66f98216af608773e3d8bd6b0ebfe
MuhammedHaji <138349773+MuhammedHaji@users.noreply.github.com>
2025-10-19 11:12:08 +0300

    Add playRound() and scoring logic

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ function getHumanChoice(){
     return input.toLowerCase();
 
 }
+let humaScore=0;
+let computerScore=0;
+//playRound
+function playRound(humanChoice,computerChoice){
+    if(!humanChoice){
+        console.log("No human choice (cancelled).Round skipped.");
+        return;
+    }
+    humanChoice=humanChoice.toLowerCase();
+    if(humanChoice===computerChoice){
+        console.log(`It's a tie both chose ${humanChoice}`);
+        return;
+    }
+    const humanWins=(humanChoice==="rock" && computerChoice==="scissors") ||
+    (humanChoice==="paper" && computerChoice==="rock") ||
+    (humanChoice==="scissors" && computerChoice==="paper");
+    if(humanWins){
+        humaScore++;
+        console.log(`you win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`)
+    } else {
+        computerScore++;
+        console.log(`you lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`)
+    }
+    console.log(`score-> you: ${humaScore}| computer: ${computerScore}`);
+
+}
+function capitalize(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1);
+}
